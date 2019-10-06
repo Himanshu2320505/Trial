dcae291939998a1ae5b5f64bc37c3984c9614bd6
HIMANSHU SEKHARDAS <HIMANSHU SEKHARDAS@192.168.0.7>
2019-10-06 14:55:21 +0530

    commit

---
+++ pageobject/pageobject/src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -0,0 +1,45 @@
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+
+public class LoginPageTest extends TestBase {
+
+	LoginPage loginPage;
+	HomePage homepage;
+
+	public LoginPageTest() {
+		super(); // to call the parent class constructor -TestBase()
+	}
+
+	@BeforeMethod
+	public void setUp() {
+
+		initialization(); // from testbase class
+		loginPage = new LoginPage();
+	}
+
+	@Test(priority = 1)
+	public void LoginPageTest() { // constructor
+		String title = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Cogmento CRM");
+
+	}
+
+	@Test(priority = 2)
+	public void loginTest() {
+		homepage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.close();
+	}
+}
---
+++ pageobject/src/main/java/com/crm/qa/pages/LoginPage.java
@@ -0,0 +1,44 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.base.TestBase;
+
+public class LoginPage extends TestBase {
+	
+	//Page Factories or Page repositories
+	@FindBy(name="email")
+	WebElement username ;
+	
+	@FindBy(name="password")
+	WebElement password ;
+	
+	@FindBy(xpath="//div[text()='Login']")
+	WebElement loginBtn;
+	
+	@FindBy(xpath="//a[contains(text(),'Sign Up')]")
+	WebElement SignUpBtn ;
+
+	//initializing the page objects 
+	public LoginPage() { //constructor
+		PageFactory.initElements(driver, this);// thhrough initElements we initiazlize page factories
+	}
+	
+	//actions(different features available on login page)
+	public String validateLoginPageTitle() {
+		String title  = driver.getTitle();
+		return title;
+		
+	}
+	
+	public HomePage login(String un, String pw) {
+		username.sendKeys(un);
+		password.sendKeys(pw);
+		loginBtn.click();
+		
+		return new HomePage() ;
+	}
+}
